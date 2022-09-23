d71d78758bbb51dbc004e42c1428315e75a0d81e
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2022-09-23 08:39:01 +0700

    add page watch

--- components/Episode.js
+++ components/Episode.js
@@ -1,6 +1,6 @@
 function Episode({ eps, date }) {
   return (
-    <div className="flex items-end justify-between cursor-pointer">
+    <div className="flex items-end justify-between cursor-pointer hover:bg-gray-100">
       <div className="flex flex-col space-y-1">
         <h4 className="text-gray-700 font-bold">{eps}</h4>
         <span className="text-xs">{date}</span>
