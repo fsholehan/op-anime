90a6dbb715ff1a449a6b8f7f08c6b6714031d897
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2022-10-01 06:14:44 +0700

    add 404 design

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="w-full h-screen flex items-center justify-center flex-col space-y-3">
+      <h1 className="font-extrabold text-2xl animate-pulse text-gray-700">
+        303
+      </h1>
+      <span className="text-gray-500 font-medium">Not Found</span>
+      <Link href="/">
+        <a className="text-blue-500">Back to home</a>
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound;
