67eb3cebf1448033803a2ee6c517d30f609dfb84
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2022-10-03 06:25:48 +0700

    add reverse episode part 2

--- components/ListEpisode.js
+++ components/ListEpisode.js
@@ -1,12 +1,10 @@
 import Episode from "./Episode";
 
 function ListEpisode({ episodes, isReverse }) {
-  console.log(episodes.slice(0, 10).reverse());
   return (
     <div className="mt-6 flex flex-col space-y-2 divide-y">
       {isReverse
         ? episodes
-
             .map((eps, i) => (
               <Episode
                 key={i}
