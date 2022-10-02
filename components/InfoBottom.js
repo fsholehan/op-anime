d4316db093fb17ab4f6cd5304f647e7004a8ee3f
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2022-10-03 06:21:46 +0700

    add reverse episode

--- components/InfoBottom.js
+++ components/InfoBottom.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ListEpisode from "./ListEpisode";
+import { ArrowsUpDownIcon } from "@heroicons/react/24/outline";
 
 function InfoBottom({ listEps }) {
+  const [reverse, setReverse] = useState(false);
+
+  const reverseHandler = () => {
+    setReverse(!reverse);
+  };
+
   return (
     <div className="mt-10">
-      <h1 className="text-lg text-gray-700 font-semibold">Daftar Episode</h1>
-      <ListEpisode episodes={listEps} />
+      <div className="flex items-center justify-between">
+        <h1 className="text-lg text-gray-700 font-semibold">Daftar Episode</h1>
+        <button onClick={reverseHandler}>
+          <ArrowsUpDownIcon className="h-6" />
+        </button>
+      </div>
+
+      <ListEpisode episodes={listEps} isReverse={reverse} />
     </div>
   );
 }
--- components/ListEpisode.js
+++ components/ListEpisode.js
@@ -1,17 +1,31 @@
 import Episode from "./Episode";
 
-function ListEpisode({ episodes }) {
+function ListEpisode({ episodes, isReverse }) {
+  console.log(episodes.slice(0, 10).reverse());
   return (
     <div className="mt-6 flex flex-col space-y-2 divide-y">
-      {episodes?.map((eps, i) => (
-        <Episode
-          key={i}
-          eps={eps.eps}
-          date={eps.date}
-          title={eps.title}
-          slug={eps.slug}
-        />
-      ))}
+      {isReverse
+        ? episodes
+
+            .map((eps, i) => (
+              <Episode
+                key={i}
+                eps={eps.eps}
+                date={eps.date}
+                title={eps.title}
+                slug={eps.slug}
+              />
+            ))
+            .reverse()
+        : episodes?.map((eps, i) => (
+            <Episode
+              key={i}
+              eps={eps.eps}
+              date={eps.date}
+              title={eps.title}
+              slug={eps.slug}
+            />
+          ))}
     </div>
   );
 }
